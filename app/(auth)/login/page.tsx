541d09b5626d05e2b12980bc6ff247b7aff4ec53
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2025-01-05 09:09:21 -0300

    feat: login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
-const Login = () => {
+import { LoginForm } from "@/components/login-form";
+
+function Login() {
   return (
-    <section>
-      <div>Login</div>
-    </section>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
   );
-};
+}
 
 export default Login;
